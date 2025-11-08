b4cd01bcf3a950542303bac8ddd3cd215a0c3979
baronsmv <baronsmv@gmail.com>
2025-11-08 11:08:53 -0600

    Using nginx in container and uvicorn.

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "[START] Copiando archivos estáticos al servidor..."
+python manage.py collectstatic --noinput
+
 echo "[START] Aplicando makemigrations..."
 python manage.py makemigrations
 
@@ -7,4 +10,4 @@ echo "[START] Aplicando migrate..."
 python manage.py migrate
 
 echo "[START] Iniciando servidor Django..."
-python manage.py runserver 0.0.0.0:8000
+gunicorn --bind 0.0.0.0:8000 navi.wsgi:application
